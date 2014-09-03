814fe703f1a2ffd70962c11c5b556d96861ccf0c
mpiechotta <michael.piechotta@age.mpg.de>
2014-09-03 21:43:23 +0200

    Implemented methods.

--- src/accusa2/pileup/iterator/TwoSampleStrandedIterator.java
+++ src/accusa2/pileup/iterator/TwoSampleStrandedIterator.java
@@ -21,20 +21,95 @@ public class TwoSampleStrandedIterator extends AbstractTwoSampleIterator {
 
 	@Override
 	public boolean hasNext() {
-		// TODO Auto-generated method stub
+		while (hasNextA() && hasNextB()) {
+			final int compare = new Integer(genomicPositionA).compareTo(genomicPositionB);
+
+			switch (compare) {
+
+			case -1:
+				// adjust actualPosition; instead of iterating jump to specific
+				// position
+				adjustCurrentGenomicPosition(genomicPositionB, pileupBuildersA);
+				genomicPositionA = genomicPositionB;
+				break;
+
+			case 0:
+				final STRAND strandA = parallelPileup.getStrandA();
+				final STRAND strandB = parallelPileup.getStrandB();
+
+				parallelPileup.setPosition(genomicPositionA);
+				
+				// complement bases if one sample is unstranded and 
+				// the other is stranded and maps to the opposite strand
+				parallelPileup.setPileupsA(getPileups(genomicPositionA, strandA, pileupBuildersA));
+				if(strandA == STRAND.UNKNOWN && strandB == STRAND.REVERSE) {
+					parallelPileup.setPileupsA(complementPileups(parallelPileup.getPileupsA()));
+				}
+				parallelPileup.setPileupsB(getPileups(genomicPositionB, strandB, pileupBuildersB));
+				if(strandB == STRAND.UNKNOWN && strandA == STRAND.REVERSE) {
+					parallelPileup.setPileupsB(complementPileups(parallelPileup.getPileupsB()));
+				}
+
+				final boolean isVariant = isVariant(parallelPileup);
+				if (isVariant) {
+					return true;
+				} else {
+					advance();
+				}
+				break;
+
+			case 1:
+				// adjust actualPosition; instead of iterating jump to specific
+				// position
+				adjustCurrentGenomicPosition(genomicPositionA, pileupBuildersB);
+				genomicPositionB = genomicPositionA;
+				break;
+			}
+		}
+
 		return false;
 	}
 
 	@Override
 	public ParallelPileup next() {
-		// TODO Auto-generated method stub
-		return null;
+		if (! hasNext()) {
+			return null;
+		}
+
+		parallelPileup.setFilterCountsA(getCounts(genomicPositionA, strandA, pileupBuildersA));
+		parallelPileup.setFilterCountsB(getCounts(genomicPositionB, strandB, pileupBuildersB));
+
+		// advance to the next position
+		advance();
+
+		return parallelPileup;
 	}
 
 	@Override
 	protected void advance() {
-		// TODO Auto-generated method stub
-		
+		if (strandA == STRAND.UNKNOWN) {
+			if (strandB == STRAND.UNKNOWN || strandB == STRAND.REVERSE) {
+				genomicPositionA++;
+				genomicPositionB++;
+			} else if (strandB == STRAND.FORWARD){
+				strandB = STRAND.REVERSE;
+			}
+		}
+		if (strandB == STRAND.UNKNOWN) {
+			if (strandA == STRAND.REVERSE) {
+				genomicPositionA++;
+				genomicPositionB++;
+			} else if (strandA == STRAND.FORWARD){
+				strandA = STRAND.REVERSE;
+			}
+		}
+		if (strandA == STRAND.FORWARD && strandB == STRAND.FORWARD) {
+			strandA = STRAND.REVERSE;
+			strandB = STRAND.REVERSE;
+		} else {
+			genomicPositionA++;
+			genomicPositionB++;
+		}
 	}
 
 	@Override
