1b77ead500997fffd297d2cc6bbefc153bb32ad8
mpiechotta <michael.piechotta@age.mpg.de>
2014-09-05 13:32:08 +0200

    Fixed complements

--- src/accusa2/pileup/BaseConfig.java
+++ src/accusa2/pileup/BaseConfig.java
@@ -1,5 +1,7 @@
 package accusa2.pileup;
 
+import java.util.Arrays;
+
 public class BaseConfig {
 
 	// dictionary to convert byte to int -> index to ALL, or valid
@@ -21,30 +23,29 @@ public class BaseConfig {
 
 	//									  0	   1	2	 3	  4
 	public static final char[] ALL = { 'A', 'C', 'G', 'T', 'N' };
-	public static final int[] INT_COMPLEMENTED = { 3, 2, 1, 0, 4 };
+//	public static final int[] INT_COMPLEMENTED = { 3, 2, 1, 0, 4 };
 
 	// this bases are the one that are used for computation
 	public static final char[] VALID = { 'A', 'C', 'G', 'T' };
 	public static final char[] VALID_COMPLEMENTED = { 'T', 'G', 'C', 'A' };
 
+	private int[] VALIDbyte2int;
+	
 	private char[] bases;
-	public int[] byte2int;
+	private int[] byte2int;
 	// complement
-	public int[] complementByte2int;
+	private int[] complementByte2int;
 
 	public BaseConfig(char[] bases) {
-		this.bases = bases;
-		byte2int = Byte2baseI(bases);
-		complementByte2int = complementByte2baseI(bases);
+		VALIDbyte2int = Byte2baseI(VALID);
+		
+		setBases(bases);
 	}
 
 	// rearrange depending on given bases
 	public int[] Byte2baseI(final char[] bases) {
 		final int[] byte2int = new int[BYTE_BASE2INT_BASE.length];
-
-		for (int i = 0; i < byte2int.length; ++i) {
-			byte2int[i] = -1;
-		}
+		Arrays.fill(byte2int, -1);
 
 		for (int i = 0; i < bases.length; ++i) {
 			byte2int[(int)bases[i]] = i;
@@ -60,26 +61,25 @@ public class BaseConfig {
 	}
 
 	private char getComplementBase(char base) {
-		for (int i = 0; i < VALID.length; ++i) {
-			if (VALID[i] == base) {
-				return VALID_COMPLEMENTED[i];
-			}
+		int baseI = VALIDbyte2int[(byte)base];
+		if (baseI < 0) {
+			return 'N';
 		}
-
-		return 'N';
+		return VALID_COMPLEMENTED[baseI];
 	}
 
+	public int getComplementBaseI(byte base) {
+		return complementByte2int[base];
+	}
+	
 	// complement
 	public int[] complementByte2baseI(final char[] bases) {
 			final int[] byte2int = new int[BYTE_BASE2INT_BASE.length];
-
-			for (int i = 0; i < byte2int.length; ++i) {
-				byte2int[i] = -1;
-			}
+			Arrays.fill(byte2int, -1);
 
 			for (int i = 0; i < bases.length; ++i) {
 				char base = getComplementBase(bases[i]);
-				byte2int[(int)base] = i;
+				byte2int[(int)base] = getBaseI((byte)bases[i]);
 			}
 			return byte2int;
 		}
@@ -89,10 +89,6 @@ public class BaseConfig {
 		return byte2int[base];
 	}
 
-	public int getComplementBaseI(byte base) {
-		return complementByte2int[base];
-	}
-
 	public char[] getBases() {
 		return bases;
 	}
@@ -100,5 +96,6 @@ public class BaseConfig {
 	public void setBases(char[] bases) {
 		this.bases = bases;
 		byte2int = Byte2baseI(bases);
+		complementByte2int = complementByte2baseI(bases);
 	}
 }
