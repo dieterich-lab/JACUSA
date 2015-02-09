45e64657bcb0b2c6be4b115105b9d7861a3de02a
michael <michael.piechotta@gmail.com>
2015-02-09 14:48:20 +0100

    Fixed Distance Filter description

--- src/jacusa/filter/factory/DistanceFilterFactory.java
+++ src/jacusa/filter/factory/DistanceFilterFactory.java
@@ -35,7 +35,7 @@ public class DistanceFilterFactory extends AbstractFilterFactory<WindowCache> {
 		super(
 				'D', 
 				// TODO "Filter distance to Intron and INDEL position. Default: " + DISTANCE + ":" + MIN_RATIO + ":" + MIN_COUNT +" (D:distance:min_ratio:min_count)", 
-				"Filter distance to Intron and INDEL position. Default: " + DISTANCE + ":" + MIN_RATIO + " (D:distance:min_ratio)",
+				"Filter distance to Read Start/End, Intron, and INDEL position. Default: " + DISTANCE + ":" + MIN_RATIO + " (D:distance:min_ratio)",
 				true,
 				cigarOperator);
 		this.parameters = parameters;
