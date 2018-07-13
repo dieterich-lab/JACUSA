496e0716c2e8e452f8d98115e0f2300db8bf1aa9
michael <michael.piechotta@gmail.com>
2018-07-13 18:04:52 +0200

    Version bump

--- src/jacusa/JACUSA.java
+++ src/jacusa/JACUSA.java
@@ -57,7 +57,7 @@ public class JACUSA {
 	private static SimpleTimer timer;
 	public static final String NAME = "jacusa";	
 	public static final String JAR = NAME + ".jar";
-	public static final String VERSION = "1.2.3-TEST1";
+	public static final String VERSION = "1.2.3";
 
 	// command line interface
 	private CLI cli;
