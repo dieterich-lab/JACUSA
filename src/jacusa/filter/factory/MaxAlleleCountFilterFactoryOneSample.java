35a02489529857a9b58984ce5fa38ce5a9f41b87
Michael Piechotta <michael.piechotta@gmail.com>
2019-03-21 10:06:10 +0100

    Added one condition specific MaxAlleleCountFilter

---
+++ src/jacusa/filter/factory/MaxAlleleCountFilterFactoryOneSample.java
@@ -0,0 +1,73 @@
+package jacusa.filter.factory;
+
+import jacusa.cli.parameters.SampleParameters;
+import jacusa.filter.AbstractStorageFilter;
+import jacusa.filter.storage.DummyFilterFillCache;
+import jacusa.pileup.ParallelPileup;
+import jacusa.pileup.Result;
+import jacusa.pileup.iterator.AbstractWindowIterator;
+import jacusa.util.Location;
+import jacusa.util.WindowCoordinates;
+
+public class MaxAlleleCountFilterFactoryOneSample extends AbstractFilterFactory<Void> {
+
+	private static int ALLELE_COUNT = 2;
+	private int alleleCount;
+	
+	public MaxAlleleCountFilterFactoryOneSample() {
+		super(
+				'M', 
+				"Max allowed alleles per parallel pileup. Default: "+ ALLELE_COUNT);
+		alleleCount = ALLELE_COUNT;
+	}
+	
+	@Override
+	public DummyFilterFillCache createFilterStorage(
+			WindowCoordinates windowCoordinates,
+			SampleParameters sampleParameters) {
+		return new DummyFilterFillCache(getC());
+	}
+
+	@Override
+	public AbstractStorageFilter<Void> createStorageFilter() {
+		return new MaxAlleleFilter(getC());
+	}
+
+	@Override
+	public void processCLI(String line) throws IllegalArgumentException {
+		if (line.length() == 1) {
+			return;
+		}
+
+		final String[] s = line.split(Character.toString(AbstractFilterFactory.SEP));
+
+		for (int i = 1; i < s.length; ++i) {
+			switch(i) {
+
+			case 1:
+				final int alleleCount = Integer.valueOf(s[i]);
+				if (alleleCount < 0) {
+					throw new IllegalArgumentException("Invalid allele count " + line);
+				}
+				this.alleleCount = alleleCount;
+				break;
+				
+			default:
+				throw new IllegalArgumentException("Invalid argument: " + line);
+			}
+		}
+	}
+	
+	private class MaxAlleleFilter extends AbstractStorageFilter<Void> {
+		public MaxAlleleFilter(final char c) {
+			super(c);
+		}
+		
+		@Override
+		public boolean filter(final Result result, final Location location, final AbstractWindowIterator windowIterator) {
+			final ParallelPileup parallelPileup = result.getParellelPileup();
+			return parallelPileup.getPooledPileup1().getAlleles().length > alleleCount;
+		}
+	}
+	
+}
